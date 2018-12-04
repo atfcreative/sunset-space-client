f1f156c4d0f2e8d593ba272881b7ce829fb1c34c
kbbushman <k.bushman@yahoo.com>
2018-12-04 14:28:51 -0800

    Updates

--- src/constants/stripe.js
+++ src/constants/stripe.js
@@ -3,7 +3,8 @@
 // const MY_PUBLISHABLE_KEY = process.env.MY_PUBLISHABLE_KEY;
 
 // const STRIPE_PUBLISHABLE = 'pk_test_PIxI2b4ctUOU8W2WcIP3aCTa';
-const STRIPE_PUBLISHABLE = 'sk_test_21sbK2K869BnJJPOm3BGpKcd';
+// const STRIPE_PUBLISHABLE = 'sk_test_21sbK2K869BnJJPOm3BGpKcd';
+const STRIPE_PUBLISHABLE =  'pk_test_PIxI2b4ctUOU8W2WcIP3aCTa';
 
 // const STRIPE_PUBLISHABLE = process.env.NODE_ENV === 'production'
 // ? 'pk_live_MY_PUBLISHABLE_KEY'
