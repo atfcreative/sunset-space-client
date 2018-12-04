13d4c9863ae855dee0b011b906f5eee8b4a28713
atfcreative <atfcreative@gmail.com>
2018-12-04 15:04:13 -0800

    adds profile endpoints

--- src/components/forms/SignUpForm.js
+++ src/components/forms/SignUpForm.js
@@ -28,7 +28,7 @@ class SignUpForm extends Component {
           username: this.state.username,
           password: this.state.password,
         }
-        axios.post('https://ss-test-server.herokuapp.com/api/users/register', newUser)
+        axios.post('https://sunset-space-server.herokuapp.com/api/users/register', newUser)
           .then(res => {
             this.props.history.push('/signin')
           })
