c82235e19e47e7cf063e70b7b1acf547b64d661b
atfcreative <atfcreative@gmail.com>
2018-12-04 10:56:24 -0800

    Updates stripe config

--- src/constants/stripe.js
+++ src/constants/stripe.js
@@ -1,9 +1,11 @@
 // Use dotenv to read .env vars into Node
-require('dotenv').config();
+// require('dotenv').config();
 // const MY_PUBLISHABLE_KEY = process.env.MY_PUBLISHABLE_KEY;
 
-const STRIPE_PUBLISHABLE = process.env.NODE_ENV === 'production'
-? 'pk_live_MY_PUBLISHABLE_KEY'
-: 'pk_test_PIxI2b4ctUOU8W2WcIP3aCTa';
+const STRIPE_PUBLISHABLE = 'pk_test_PIxI2b4ctUOU8W2WcIP3aCTa';
+
+// const STRIPE_PUBLISHABLE = process.env.NODE_ENV === 'production'
+// ? 'pk_live_MY_PUBLISHABLE_KEY'
+// : 'pk_test_PIxI2b4ctUOU8W2WcIP3aCTa';
 
 export default STRIPE_PUBLISHABLE;
